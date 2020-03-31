a612d4b547444bb828b84cc462cf7cb44d0a1661
zenghui <240164005@qq.com>
2020-03-31 11:59:24 +0800

    xx

--- promisify.js
+++ promisify.js
@@ -1,6 +1,10 @@
 const util = require('util');
 const fs = require('fs');
- 
+
+// 只要符合 Node.js 的回调风格，所有函数都可以这样转换。也就是说，只要满足下面两个条件，无论是不是原生方法，都可以：
+
+// 最后一个参数是回调函数
+// 回调函数的参数为 (err, result)，前面是可能的错误，后面是正常的结果
 // 返回一个文件的详细信息
 const stat = util.promisify(fs.stat);
 // promise用法
@@ -38,3 +42,5 @@ async function main() {
     }
 }
 main()
+
+// 对于非 Node.js 标准风格的函数，也提供自定义转换函数的功能。
